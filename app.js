40cda629cccb11f53a5ac42d323fc2ed6cc30363
lightnonstop <umichaeledet003@gmail.com>
2023-02-08 22:02:48 +0000

    Contains table data

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 let previousVal = "";
 let currentVal = "";
 let op = "";
-let store = 0;
+let updateScreen = "";
+
+
+const allElements = document.querySelectorAll('*');
+
+console.log(allElements.forEach((element) => {
+    element.style.userSelect = "none";
+}));
 
 
 /* Stores document element */
-const printOnScreen = document.querySelector('#print');
+const primaryScreen = document.querySelector('#primary-screen');
+const secondaryScreen = document.querySelector('#secondary-screen');
 const digits = document.querySelectorAll('.digit');
 const deleteKey = document.querySelector('#delete');
 const operators = document.querySelectorAll('.operator');
@@ -15,142 +23,189 @@ const equal = document.querySelector('#equal');
 const keys = document.querySelectorAll('.keys');
 
 
-printOnScreen.style.userSelect = "none";
+primaryScreen.textContent = "0";
+secondaryScreen.textContent = "0";
 
-/* Listens for user interaction with elements */
 keys.forEach((key) => key.addEventListener('mousedown', (e) => {
-    e.target.style.opacity = "0.7";
-    e.target.style.backgroundColor = "red";
+    e.target.style.backgroundColor = "grey";
 }))
 
 keys.forEach((key) => key.addEventListener('mouseup', (e) => {
-    e.target.style.opacity = "";
     e.target.style.backgroundColor = "";
 }))
 
-printOnScreen.textContent = "0";
-
 digits.forEach((digit) => digit.addEventListener('click', (e) => {
-    handleNum(e.target.textContent);
-    printOnScreen.textContent = currentVal;
+    handleDigit(e.target.textContent);
+    if (previousVal !== "")
+    {
+        secondaryScreen.textContent = currentVal + " " + updateScreen;
+        primaryScreen.textContent = currentVal;
+    }
+    else
+    {
+        primaryScreen.textContent = currentVal;
+    }
 }))
 
 operators.forEach((operator) => operator.addEventListener('click', (e) => {
     handleSign(e.target.textContent);
+    updateScreen = op + " " + previousVal;
+    secondaryScreen.textContent =  updateScreen;
+    primaryScreen.textContent = previousVal;
+
 }))
 
-equal.addEventListener('click', () => {
-    operate();
-    if (store.length <= 8)
-        printOnScreen.textContent = store;
+equal.addEventListener('click', (e) => {
+    operateWhenEqual();
+    primaryScreen.textContent = previousVal;
+    if (previousVal.length <= 8)
+        primaryScreen.textContent = previousVal;
     else
     {
-        printOnScreen.textContent = ".." + store.slice(0,12);
+        primaryScreen.textContent = ".." + previousVal.slice(0, 16);
     }
 })
 
-reset.addEventListener('click', () => {
-    currentVal = "";
-    printOnScreen.textContent = "0";
-    previousVal = "";
-    op = "";
+equal.addEventListener('mousedown', (e) => {
+    primaryScreen.style.color = "orangered";
 })
+
+equal.addEventListener('mouseup', (e) => {
+    primaryScreen.style.color = "";
+})
+
+reset.addEventListener('click', () => {
+    previousVal = "";
+    currentVal = "";
+    op = "";
+    updateScreen = "";
+    primaryScreen.textContent = "0";
+    secondaryScreen.textContent = "0";
+})
+
+
+deleteKey.addEventListener('click', () => {
+    primaryScreen.textContent = currentVal.slice(0, -1);
+    currentVal = primaryScreen.textContent;
+
+    if (currentVal.length === 0)
+    {
+        previousVal = "";
+        currentVal = "";
+        op = "";
+        updateScreen = "";
+        primaryScreen.textContent = "0";
+        secondaryScreen.textContent = "0";
+    }
+})
+
 
 decimal.addEventListener('click', () => {
     addDecimal();
 })
 
-deleteKey.addEventListener('click', () => {
-    printOnScreen.textContent = currentVal.slice(0, -1);
-    currentVal = printOnScreen.textContent;
-    if (currentVal.length === 0)
-    {
-        currentVal = "";
-        printOnScreen.textContent = "0";
-        previousVal = "";
-        op = "";
-    }
-})
-
-/* Handles value output */
-function handleNum(num)
+function handleDigit(digit)
 {
     if (currentVal.length <= 14)
     {
-        currentVal += num;
+        currentVal += digit;
     }
 }
 
-/* Assigns an operator for mathematical operations*/
+
 function handleSign(sign)
 {
-    if (previousVal.length > 0)
+    if (previousVal !== "")
     {
-        previousVal = store;
+        operate();
     }
-    else
+    else 
     {
+        op = sign;
         previousVal = currentVal;
     }
     op = sign;
     currentVal = "";
 }
 
-/* Carries out mathematical operations */
 function operate()
 {
     previousVal = Number(previousVal);
     currentVal = Number(currentVal);
     if (op === "+")
-        add();
-    else if (op === "x")
-        mult();
-    else if (op === "-")
-        subtract();
-    else if (op === "/")
-        divide();
-
-    previousVal = store;
-
-    if (store < 0)
     {
-        store = store * -1;
-        store = store.toString() + "-";
+        previousVal += currentVal;
+    }
+    else if (op === "-")
+    {
+        if (previousVal < 0)
+            previousVal = -1 * (-1 * previousVal) + (-1 * currentVal);
+        else
+            previousVal = previousVal + (-1 * currentVal);
+    }
+    else if (op === "/")
+    {
+        previousVal /= currentVal;
+    }
+    else if (op === "×")
+    {
+        previousVal *= currentVal;
+    }
+    if (previousVal < 0)
+    {
+        previousVal *= -1;
+        previousVal = previousVal + "-";
     }
     else
-        store = store.toString();
-    previousVal = previousVal.toString();
+        previousVal = previousVal.toString();
     currentVal = currentVal.toString();
-
-    console.log(store);
 }
 
-/* Adds values */
-function add()
+function operateWhenEqual()
 {
-    store = previousVal + currentVal;
-}
-/* Multiplies values */
-function mult()
-{
-    store = previousVal * currentVal;
-}
-/* Subtracts values */
-function subtract()
-{
-    store = previousVal + (-1 * currentVal);
-}
-/* Divides values */
-function divide()
-{
-    store = previousVal / currentVal;
+    previousVal = Number(previousVal);
+    currentVal = Number(currentVal);
+    if (op === "+")
+    {
+        previousVal += currentVal;
+        currentVal = 0;
+    }
+    else if (op === "-")
+    {
+        if (previousVal < 0)
+            previousVal = -1 * (-1 * previousVal) + (-1 * currentVal);
+        else
+            previousVal = previousVal + (-1 * currentVal);
+        currentVal = 0;
+    }
+    else if (op === "/")
+    {
+        previousVal /= currentVal;
+        currentVal = 1;
+    }
+    else if (op === "×")
+    {
+        previousVal *= currentVal;
+        currentVal = 1;
+    }
+    if (previousVal < 0)
+    {
+        previousVal *= -1;
+        previousVal = previousVal + "-";
+    }
+    else
+        previousVal = previousVal.toString();
+    currentVal = currentVal.toString();
 }
 
-/* Adds decimal point */
 function addDecimal()
 {
-    if (!currentVal.includes("."))
+    if ((!currentVal.includes('.')) && currentVal.length > 0)
     {
-        currentVal += ".";
+        currentVal += '.';
+    }
+    else if (!currentVal.includes('.') && currentVal.length === 0)
+    {
+        currentVal += "0."
     }
 }
